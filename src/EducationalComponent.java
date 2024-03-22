76127ef1701c9c897cc749847c673d704ba21db5
Lucas Sebastião <75097274+lmsebastiao@users.noreply.github.com>
2024-03-22 11:37:06 +0000

    course, lesson

--- src/EducationalComponent.java
+++ src/EducationalComponent.java
@@ -1,2 +1,13 @@
-public class EducationalComponent {
+import java.util.ArrayList;
+
+public abstract class EducationalComponent {
+    public void addComponent(Lesson lesson) {
+
+    }
+
+    public abstract void addComponent(EducationalComponent component);
+
+    public void display() {
+
+    }
 }
