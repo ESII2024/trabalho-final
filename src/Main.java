d6c6b4635204e72bddf9f5d72b524a9616002c64
Lucas Sebastião <75097274+lmsebastiao@users.noreply.github.com>
2024-04-05 11:36:28 +0100

    logs

--- src/Main.java
+++ src/Main.java
@@ -4,52 +4,61 @@ import Singleton.ConfigurationManager;
 public class Main {
     public static void main(String[] args) {
         Teste();
-        SetupConstants();
     }
 
-    private static void SetupConstants() {
-        ConfigurationManager configManager = ConfigurationManager.getInstance();
-        configManager.setConfiguration("MAX_POOL_SIZE", 10);
-        //System.out.println("MAX_POOL_SIZE " + configManager.getConfiguration("MAX_POOL_SIZE"));
-    }
-
-    private static void Teste() {
+    private static void SingletonPattern() {
         // Test Singleton pattern - DANIEL
+        System.out.println("\nSingletonPattern");
         ConfigurationManager configManager = ConfigurationManager.getInstance();
         configManager.setConfiguration("app.name", "E-Learning App");
         configManager.setConfiguration("app.version", "1.0");
         System.out.println("App Name: " + configManager.getConfiguration("app.name"));
         System.out.println("App Version: " + configManager.getConfiguration("app.version"));
+    }
 
+    private static void FactoryPattern() {
         // Test Factory pattern - DANIEL
+        System.out.println("\nFactoryPattern");
         EducationalObject quiz = EducationalObjectFactory.createObject("quiz");
         EducationalObject video = EducationalObjectFactory.createObject("video");
         EducationalObject article = EducationalObjectFactory.createObject("article");
         quiz.display();
         video.display();
         article.display();
+    }
 
+    private static void BridgePattern() {
         // Test Bridge pattern - MIGUEL
+        System.out.println("\nBridgePattern");
         Educational.EducationalContent quizWeb = new BridgePattern.Quiz(new Platform.WebPlatform());
         Educational.EducationalContent quizMobile = new BridgePattern.Quiz(new Platform.MobilePlatform());
         quizWeb.display();
         quizMobile.display();
+    }
 
+    private static void CompositePattern() {
         // Test Composite pattern - LUCAS
+        System.out.println("\nCompositePattern");
         EducationalComponent lesson1 = new Lesson("Educational.Lesson 1");
         EducationalComponent lesson2 = new Lesson("Educational.Lesson 2");
         EducationalComponent course1 = new Course("Educational.Course 1");
         course1.addComponent(lesson1);
         course1.addComponent(lesson2);
         course1.display();
+    }
 
+    private static void ObjectPoolPattern() {
         // Test Object Pool pattern - JOAO
+        System.out.println("\nObjectPoolPattern");
         DatabaseConnection connection1 = DatabaseConnection.getConnection();
         DatabaseConnection connection2 = DatabaseConnection.getConnection();
         connection1.releaseConnection(connection1);
         connection2.releaseConnection(connection2);
+    }
 
+    private static void MementoPattern() {
         // Test Memento pattern - JOAO
+        System.out.println("\nMementoPattern");
         Course myCourse = new Course("My Educational.Course");
         CourseProgress progress1 = new CourseProgress("Educational.Lesson 1", 50);
         myCourse.setProgress(progress1);
@@ -63,12 +72,35 @@ public class Main {
         myCourse.restoreProgress(savedProgress);
         System.out.println("Current Educational.Lesson: " + myCourse.getProgress().getCurrentLesson());
         System.out.println("Educational.Lesson Progress: " + myCourse.getProgress().getLessonProgress());
+    }
 
+    private static void DecoratorPattern() {
         // Test Decorator pattern - LUCAS | verificar se esta bem
+        System.out.println("\nDecoratorPattern");
         EducationalObject quizWithBadge = new BadgeDecorator(new Quiz());
         quizWithBadge.display();
         EducationalObject quizWithCertificate = new CertificateDecorator(quizWithBadge);
         quizWithCertificate.display();
+    }
+
+    private static void Teste() {
+        System.out.println("\n\nDaniel");
+        SingletonPattern();
+
+        FactoryPattern();
+
+        System.out.println("\n\nMiguel");
+        BridgePattern();
+
+        System.out.println("\n\nJoão");
+        ObjectPoolPattern();
+
+        MementoPattern();
+
+        System.out.println("\n\nLucas");
+        CompositePattern();
+
+        DecoratorPattern();
     }
 }
 
